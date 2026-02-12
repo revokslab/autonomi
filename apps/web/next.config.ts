1610473fe3509af1954ebb75828eb67fe41590c0
irere123 <irere2020@outlook.com>
2026-02-12 20:18:11 +0200

    fix: faced limits on cloudflare

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,4 @@
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
-
-initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
 	output: "standalone",
